1af8aedfa98d9b856e496293c2d9ab5856644cb7
Gustavo Costa <gcostaa.arruda@gmail.com>
2023-08-07 17:57:17 -0300

    S e O do SOLID

--- formacao-boas-praticas-php/phpunit-tdd/leilao/Teste/Model/TestLeilao.php
+++ formacao-boas-praticas-php/phpunit-tdd/leilao/Teste/Model/TestLeilao.php
@@ -6,6 +6,7 @@ use Alura\Leilao\Model\Lance;
 use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Model\Usuario;
 use PHPUnit\Framework\TestCase;
+use function PHPUnit\Framework\assertEquals;
 
 class TestLeilao extends TestCase
 {
@@ -40,6 +41,36 @@ class TestLeilao extends TestCase
         //precisamos garantir que apenas o primeiro lance seja mantido
         static::assertCount(1, $leilao->getLances());
         static::assertEquals(2000, $leilao->getLances()[0]->getValor());
+    }
+
+
+    public function testLeilaoNaoDeveAceitarMaisDeCincoLancesPorUsuario()
+    {
+
+
+        $leilao = new Leilao('Civic 2008');
+        $joao = new Usuario("Joao");
+        $maria = new Usuario("Maria");
+        $leilao->recebeLance(new Lance($joao,2000));
+        $leilao->recebeLance(new Lance($maria,2500));
+        $leilao->recebeLance(new Lance($joao,3000));
+        $leilao->recebeLance(new Lance($maria,3500));
+        $leilao->recebeLance(new Lance($joao,4000));
+        $leilao->recebeLance(new Lance($maria,4500));
+        $leilao->recebeLance(new Lance($joao,5000));
+        $leilao->recebeLance(new Lance($maria,5500));
+        $leilao->recebeLance(new Lance($joao,6000));
+        $leilao->recebeLance(new Lance($maria,6500));
+
+        //esse lance deve ser ignorado
+        $leilao->recebeLance(new Lance($joao,7000));
+        $leilao->recebeLance(new Lance($maria,7500));
+
+
+        static::assertCount(10, $leilao->getLances());
+        //garante que o ultimo lance é 7500
+        static:assertEquals(7500, $leilao->getLances()[array_key_last($leilao->getLances())]->getValor());
+
     }
     
     public static function geraLances()
