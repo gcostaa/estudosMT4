d84c3486249edc920b41b1815c4fb633cbcc8eec
Gustavo Costa <gcostaa.arruda@gmail.com>
2023-08-09 20:11:11 -0300

    Melhorando o código, criando classes e separando responsabilidades

--- php-mysql/blog/index.php
+++ php-mysql/blog/index.php
@@ -1,33 +1,9 @@
 <?php
 
-$artigos = [
+require_once "Artigo.php";
 
-    [
-        'Titulo' => 'Primeiros passos com Spring',
-        'Conteudo' => <<<CONTEUDO
-                Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-                programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.
-               CONTEUDO,
-        'Link' => 'primeiros-passos-com-spring.html'
-    ],
-
-    [
-        'Titulo' => 'O que é Metodologia Ágil',
-        'Conteudo' => <<<CONTEUDO
-                Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.
-               CONTEUDO,
-        'Link' => 'o-que-e-metodologia-agil.html'
-    ],
-
-    [
-        'Titulo' => 'Como é o funil do Growth Hacking?',
-        'Conteudo' => <<<CONTEUDO
-                Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-                mais sobre ele.
-               CONTEUDO,
-        'Link' => 'como-e-o-funil-do-growth-hacking.html'
-    ],
-];
+$artigo = new Artigo();
+$artigos = $artigo->exibirTodos();
 
 ?>
 
@@ -47,20 +23,22 @@ $artigos = [
         <?php
 
             foreach ($artigos as $artigo)
-            {?>
-                         <h2>
+            :?>
+                <h2>
                     <a href="<?php echo $artigo['Link']?>">
                         <?php
                         echo $artigo['Titulo'];
                         ?>
-                </a>
+                    </a>
+
                 </h2>
+
                 <p>
                     <?php
                     echo $artigo['Conteudo'];
                     ?>
                 </p>
-            <?php } ?>
+            <?php endforeach; ?>
     </div>
 </body>
 
